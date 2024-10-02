8573b8ab9f9e20757b9c6d682734712688c7ab50
Computer <96786190+PiKa919@users.noreply.github.com>
2024-10-02 22:18:38 +0530

    added favicon & title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <Image src="/images/nextjs.png" alt="Next.js" width={200} height={200} />
+      {/* <Image src="/images/nextjs.png" alt="Next.js" width={200} height={200} /> */}
     </div>
   );
 }
