36056e17b1fab54b2bab325fc0cd556ee3ddfc72
Computer <96786190+PiKa919@users.noreply.github.com>
2024-12-24 13:37:37 +0530

    Add @vercel/speed-insights package and import in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
