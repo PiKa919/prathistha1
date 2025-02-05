ddc702233491f9423344a947f110d5c63901c9ee
dilip1106 <dilipteli38@gmail.com>
2025-02-05 21:53:06 +0530

    scroll pages changes

--- src/components/ui/scroll-progress.tsx
+++ src/components/ui/scroll-progress.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { motion, MotionProps, useScroll, useSpring } from "motion/react";
+import { motion, MotionProps, useScroll, useSpring } from "framer-motion";
 import React from "react";
-interface ScrollProgressProps
-  extends Omit<React.HTMLAttributes<HTMLElement>, keyof MotionProps> {}
 
 export const ScrollProgress = React.forwardRef<
   HTMLDivElement,
-  ScrollProgressProps
+  Omit<React.HTMLAttributes<HTMLElement>, keyof MotionProps>
 >(({ className, ...props }, ref) => {
   const { scrollYProgress } = useScroll();
 
@@ -21,13 +19,8 @@ export const ScrollProgress = React.forwardRef<
   return (
     <motion.div
       ref={ref}
-      className={cn(
-        "fixed inset-x-0 top-0 z-[1000] h-1 origin-left bg-gradient-to-r from-[#A97CF8] via-[#F38CB8] to-[#FDCC92]",
-        className,
-      )}
-      style={{
-        scaleX,
-      }}
+      className={cn("fixed top-0 left-0 right-0 h-1 bg-blue-500", className)}
+      style={{ scaleX }}
       {...props}
     />
   );
