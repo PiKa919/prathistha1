241d703a4661d3f1bc497665f45b40b0e5e4e62a
dilip1106 <dilipteli38@gmail.com>
2025-02-05 21:49:11 +0530

    Update motion package to version 12.3.1 and refactor ScrollProgress component for improved styling

--- src/components/ui/scroll-progress.tsx
+++ src/components/ui/scroll-progress.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { motion, MotionProps, useScroll, useSpring } from "framer-motion";
+import { motion, MotionProps, useScroll, useSpring } from "motion/react";
 import React from "react";
+interface ScrollProgressProps
+  extends Omit<React.HTMLAttributes<HTMLElement>, keyof MotionProps> {}
 
 export const ScrollProgress = React.forwardRef<
   HTMLDivElement,
-  Omit<React.HTMLAttributes<HTMLElement>, keyof MotionProps>
+  ScrollProgressProps
 >(({ className, ...props }, ref) => {
   const { scrollYProgress } = useScroll();
 
@@ -19,8 +21,13 @@ export const ScrollProgress = React.forwardRef<
   return (
     <motion.div
       ref={ref}
-      className={cn("fixed top-0 left-0 right-0 h-1 bg-blue-500", className)}
-      style={{ scaleX }}
+      className={cn(
+        "fixed inset-x-0 top-0 z-[1000] h-1 origin-left bg-gradient-to-r from-[#A97CF8] via-[#F38CB8] to-[#FDCC92]",
+        className,
+      )}
+      style={{
+        scaleX,
+      }}
       {...props}
     />
   );
